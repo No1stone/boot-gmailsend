300c1abfb8fbb4ad5052389519fe1284c4ca9b95
jangwonseok <jangws1003@naver.com>
2025-04-04 10:23:55 +0900

    first commit

---
+++ src/main/java/com/origemite/gmail/bootgmailsend/BootGmailsendApplication.java
@@ -0,0 +1,13 @@
+package com.origemite.gmail.bootgmailsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGmailsendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootGmailsendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/origemite/gmail/bootgmailsend/BootGmailsendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origemite.gmail.bootgmailsend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGmailsendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
